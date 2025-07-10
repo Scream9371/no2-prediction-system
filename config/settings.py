eabc90854b6dc9a72e71f0adfa2dd4fd0792fef5
Yuxuan Mao <3135972956@qq.com>
2025-07-10 15:34:08 +0800

    使用MySQL格式

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,10 @@
 import os
 
-
 class Settings:
     API_KEY = os.getenv("HEWEATHER_API_KEY", "")
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./no2.db")
+    DATABASE_URL = os.getenv("DATABASE_URL")
     DATA_PATH = os.getenv("DATA_PATH", "./data")
     MODEL_PATH = os.getenv("MODEL_PATH", "./ml/models/trained")
     RETRAIN_THRESHOLD = float(os.getenv("RETRAIN_THRESHOLD", 0.8))
-
 
 settings = Settings()
