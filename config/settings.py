488120fdc3fd39cb028817069514b8aba53e7b5f
Scream9371 <3135972956@qq.com>
2025-07-15 01:05:59 +0800

    弃用HF_PRIVATE_KEY参数

--- config/settings.py
+++ config/settings.py
@@ -8,8 +8,9 @@ class Settings:
     RETRAIN_THRESHOLD = float(os.getenv("RETRAIN_THRESHOLD", 0.8))
     
     # JWT相关配置（和风天气商业版API）
+    # 注意：现在推荐使用 utils.auth 模块获取这些配置
     HF_API_HOST = os.getenv("HF_API_HOST")
-    HF_PRIVATE_KEY = os.getenv("HF_PRIVATE_KEY")
+    HF_PRIVATE_KEY = os.getenv("HF_PRIVATE_KEY")  # 已弃用，使用utils.auth.load_private_key()
     HF_PROJECT_ID = os.getenv("HF_PROJECT_ID")
     HF_KEY_ID = os.getenv("HF_KEY_ID")
 
