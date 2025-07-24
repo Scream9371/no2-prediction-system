afc5ddfc7484fd3fc28074d1d232b1f0d27d5212
Scream9371 <3135972956@qq.com>
2025-07-24 11:48:59 +0800

    优化模块导入格式

--- scripts/setup_database.py
+++ scripts/setup_database.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
-from database.models import Base
+
 from database.db import engine
+from database.models import Base
 
 
 def init_database():
